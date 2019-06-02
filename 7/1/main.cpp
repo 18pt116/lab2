3921e8ba96f0ac8be474e11cc76c0d6d89cd4570
18pt116 <48137616+18pt116@users.noreply.github.com>
2019-06-02 18:52:42 +0300

    Add files via upload

---
+++ 7/1/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <crypt.h>
+#include <string>
+#include <fstream>
+using namespace std;
+void findPass(string pass, const string& hash)
+{
+	crypt_data *pCryptData = new crypt_data;
+	size_t pos = hash.find_last_of('$');
+	string hashHead = hash.substr(0,pos);
+	do {
+		string
+		newHash(crypt_r(pass.data(),hashHead.data(),pCryptData));
+		if (newHash == hash) {
+			cout<<"Hash: "<<hash<<endl<<"Pass:"<<pass<<endl;
+			break;
+		}
+	} while (next_permutation( pass.begin(),pass.end() ) );
+	delete pCryptData;
+}
+int main(int argc, char *argv[])
+{
+	int n ;
+	cout << "Введите количество хешей (1,2,4,8): "<<endl;
+	cin >> n ;
+	ifstream fin("111.txt");
+	string *str = new string [n];
+	for (int i= 0 ; i < n; i++) {
+		fin>> str[i];
+	}
+	string s = "123456789";
+	for (int i= 0 ; i < n; i++) {
+		findPass(s,str[i]);
+	}
+	return 0;
+}
